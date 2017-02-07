82fb596197a79ba2b5008d4b57f5722d75690a12
Xiao Han <han.xiao@aalto.fi>
2017-02-07 15:36:21 +0000

    slight modify

--- track_registration.py
+++ track_registration.py
@@ -20,16 +20,16 @@ event_id = event_id.iloc[0]
 
 event_url = 'https://unisport.fi/yol/web/en/crud/read/reservable.json?id={}&details=true'.format(event_id)
 
-output_path = 'log/{}-{}.txt'.format('_'.join(course_name.lower().split()), date)
+output_path = '/home/cloud-user/code/unisport/log/{}-{}.txt'.format('_'.join(course_name.lower().split()), date)
 
 while True:
     p = requests.get(event_url)
     c = p.json()['items'][0]
     resv = c['reservations']
     resv_max = c['maxReservations']
-    sleep(delay_secs)
     print('write')
     with open(output_path, 'a') as f:
         f.write('{}\t{}\t{}\n'.format(resv,
                                       resv_max,
                                       datetime.now().strftime('%Y-%m-%dT%H:%M:%S')))
+    sleep(delay_secs)
